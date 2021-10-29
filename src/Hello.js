43add90529d46f587778ff27c5e2bf0e4ea854b1
fferum <vfndtq2001@gmail.com>
2021-10-29 21:34:18 +0200

    renderList. Повторяю урок

--- src/Hello.js
+++ src/Hello.js
@@ -3,18 +3,26 @@ import React, { Component } from "react";
 export default class Hello extends Component{
     constructor(){
         super();
-        this.state = {color: "red", digit: 0};
+        this.state = {color: "red", digit: 0, list:[{title:"first"}, {title:"second"}, {title:"third"}]};
     }
 
 
     componentDidMount(){
+        console.log(this.props.parent_color)
         setTimeout(()=>{
-            this.setState({digit:10})
+            this.setState({digit:10, color: this.props.parent_color})
         },2000)
+        console.log("ComponentDidMount")
     }
 
+    renderLists(){
+        return this.state.list.map((item,index)=>{
+            return <div key={`${item.title}${index}`}>{item.title}</div>
+        });
+    }
 
     render(){
+        console.log("render")
         return (
         <div>
         <div onClick={()=>{
@@ -25,6 +33,7 @@ export default class Hello extends Component{
             Hello, world!
         </div>
         <div>{this.state.digit}</div>
+        {this.renderLists()}
         </div>
         );
     }
